0a63cf94a15cc3f2307828f61e2822d3164c4bb4
Akash1602 <yogeshgupta160293@gmail.com>
2019-11-21 20:43:44 +0530

    Update bday_nvidia.py

--- bday_notification/bday_nvidia.py
+++ bday_notification/bday_nvidia.py
@@ -1,4 +1,4 @@
-#!/home/utils/Python-3.6.1/bin/python3.6
+#!/bin/python3.6
 
 import datetime
 import email
@@ -17,14 +17,14 @@ from jinja2 import Environment, FileSystemLoader
 
 
 def Rand_Image():
-    image_list = os.listdir("/home/scratch.yogeshg_mis/project/dev/bday_notification/static/")
+    image_list = os.listdir("/static/")
     rand_image = random.choice(image_list)
-    image = os.path.join(str("/home/scratch.yogeshg_mis/project/dev/bday_notification/static/"), str(rand_image))
+    image = os.path.join(str("/static/"), str(rand_image))
     return image
 #Rand_Image()
 
 def Template_Loader():
-    template_loader = FileSystemLoader(searchpath = "/home/scratch.yogeshg_mis/project/dev/bday_notification/")
+    template_loader = FileSystemLoader(searchpath = "/bday_notification/")
     template_env = Environment(loader=template_loader)
     template = template_env.get_template("template.html")
     return template
@@ -34,9 +34,9 @@ def Send_Mail(emp_name, emp_email, message):
     image = Rand_Image()
     template = Template_Loader()
     msg = MIMEMultipart('alternative')
-    msg['From'] = 'IT India'
+    msg['From'] = ''
     msg['To'] = emp_email
-    msg['Cc'] = 'manjustaff@exchange.nvidia.com'
+    msg['Cc'] = ''
     msg['Subject'] = "Happy Birthday...!!!"
     msg.add_header('Content-Type', 'text/html')
     fp = open(image, 'rb')
@@ -48,15 +48,15 @@ def Send_Mail(emp_name, emp_email, message):
     recipient = [msg['To'] , msg['Cc']]
     msg.attach(MIMEText(template.render(name = emp_name, message = message, cid = str('image1')), "html"))
     try:
-        smtp_obj = smtplib.SMTP('smtp.nvidia.com')
-        smtp_obj.sendmail('yogeshg@nvidia.com', recipient, msg.as_string())
+        smtp_obj = smtplib.SMTP('')
+        smtp_obj.sendmail('', recipient, msg.as_string())
     except smtplib.SMTPException:
         print ("something went wrong")
 #Send_Mail()
 
 def Read_File():
-    emp_data = '/home/scratch.yogeshg_mis/project/dev/bday_notification/emp_dob.json'
-    message_data = '/home/scratch.yogeshg_mis/project/dev/bday_notification/message.json'
+    emp_data = '/bday_notification/emp_dob.json'
+    message_data = '/bday_notification/message.json'
     with open (emp_data) as file:
         emp_dict = json.loads(file.read())
     with open (message_data) as file:
